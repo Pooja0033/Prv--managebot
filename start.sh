e4c13d2f3d6d5a277c8f976b09e2c4fc265a65ce
Pooja0033 <113719724+Pooja0033@users.noreply.github.com>
2022-12-03 18:48:33 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,23 @@
+if [ -z $UPSTREAM_REPO ]
+
+then
+
+  echo "Cloning main Repository"
+
+  git clone https://github.com/Pooja0033/Prv_35.git /Prv_35
+
+else
+
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+
+  git clone $UPSTREAM_REPO /Prv_35
+
+fi
+
+cd /Prv_35
+
+pip3 install -U -r requirements.txt
+
+echo "Starting Bot...."
+
+python3 bot.py
